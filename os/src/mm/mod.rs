6511391127daf17ae591cdd690ed4fe140619051
HuangYijiaLika <23301036@bjtu.edu.cn>
2026-06-08 12:25:32 +0800

    6. 内核与应用的地址空间

--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -1,13 +1,14 @@
 mod heap_allocator;
 mod address;
 mod frame_allocator;
+mod memory_set;
 mod page_table;
 
 use address::{VPNRange, StepByOne};
 use page_table::{PTEFlags};
 pub use address::{PhysAddr, VirtAddr, PhysPageNum, VirtPageNum};
-pub use page_table::{PageTableEntry};
 pub use frame_allocator::{FrameTracker, frame_alloc};
+pub use page_table::{PageTable, PageTableEntry};
 
 pub fn init() {
     heap_allocator::init_heap();
