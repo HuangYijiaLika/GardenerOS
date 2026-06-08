56b9f9327e751ed9eae08738e4092a70e14a2a71
HuangYijiaLika <23301036@bjtu.edu.cn>
2026-06-08 17:03:33 +0800

    7. 实现基于地址空间的分时多任务

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -1,13 +1,9 @@
-use core::arch::global_asm;
+use core::arch::{asm, global_asm};
+use crate::config::{TRAP_CONTEXT, TRAMPOLINE};
 
 global_asm!(include_str!("trap.S"));
 
-pub fn init() {
-    extern "C" { fn __alltraps(); }
-    unsafe {
-        stvec::write(__alltraps as *const () as usize, TrapMode::Direct);
-    }
-}
+
 
 mod context;
 
@@ -26,6 +22,8 @@ use riscv::register::{
 
 use crate::syscall::syscall;
 use crate::task::{
+    current_user_token,
+    current_trap_cx,
     exit_current_and_run_next,
     suspend_current_and_run_next,
 };
@@ -35,8 +33,26 @@ pub fn enable_timer_interrupt() {
     unsafe { sie::set_stimer(); }
 }
 
+pub fn init() {
+    set_kernel_trap_entry();
+}
+
+fn set_kernel_trap_entry() {
+    unsafe {
+        stvec::write(trap_from_kernel as *const () as usize, TrapMode::Direct);
+    }
+}
+
+fn set_user_trap_entry() {
+    unsafe {
+        stvec::write(TRAMPOLINE as *const () as usize, TrapMode::Direct);
+    }
+}
+
 #[no_mangle]
-pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
+pub fn trap_handler() -> ! {
+    set_kernel_trap_entry();
+    let cx = current_trap_cx();
     let scause = scause::read();
     let stval = stval::read();
     match scause.cause() {
@@ -46,11 +62,7 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
         }
         Trap::Exception(Exception::StoreFault) |
         Trap::Exception(Exception::StorePageFault) => {
-            println!(
-                "[kernel] PageFault in application, bad addr = {:#x}, bad instruction = {:#x}, core dumped.",
-                stval,
-                cx.sepc
-            );
+            println!("[kernel] PageFault in application, bad addr = {:#x}, bad instruction = {:#x}, core dumped.", stval, cx.sepc);
             exit_current_and_run_next();
         }
         Trap::Exception(Exception::IllegalInstruction) => {
@@ -65,7 +77,35 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
             panic!("Unsupported trap {:?}, stval = {:#x}!", scause.cause(), stval);
         }
     }
-    cx
+    trap_return();
 }
+
+#[no_mangle]
+pub fn trap_return() -> ! {
+    set_user_trap_entry();
+    let trap_cx_ptr = TRAP_CONTEXT;
+    let user_satp = current_user_token();
+    extern "C" {
+        fn __alltraps();
+        fn __restore();
+    }
+    let restore_va = __restore as *const () as usize - __alltraps as *const () as usize + TRAMPOLINE;
+    unsafe {
+        asm!(
+            "fence.i",
+            "jr {restore_va}",
+            restore_va = in(reg) restore_va,
+            in("a0") trap_cx_ptr,
+            in("a1") user_satp,
+            options(noreturn)
+        );
+    }
+}
+
+#[no_mangle]
+pub fn trap_from_kernel() -> ! {
+    panic!("a trap from kernel!");
+}
+
 
 pub use context::TrapContext;
