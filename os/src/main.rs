7efba0dbf5cfe820b00f372f4b3cced2544635c5
HuangYijiaLika <23301036@bjtu.edu.cn>
2026-06-08 17:07:05 +0800

    8. 修改应用程序 & 9. 修改main.rs

--- os/src/main.rs
+++ os/src/main.rs
@@ -9,19 +9,18 @@ extern crate alloc;
 #[macro_use]
 extern crate bitflags;
 
-
 #[macro_use]
 mod console;
 mod lang_items;
 mod sbi;
-mod timer;
 mod syscall;
 mod trap;
 mod loader;
 mod config;
 mod task;
-mod mm;
+mod timer;
 mod sync;
+mod mm;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
@@ -31,20 +30,24 @@ fn clear_bss() {
         fn sbss();
         fn ebss();
     }
-    let sbss_ptr = sbss as *const () as usize;
-    let ebss_ptr = ebss as *const () as usize;
-    (sbss_ptr..ebss_ptr).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+    unsafe {
+        core::slice::from_raw_parts_mut(
+            sbss as *const () as usize as *mut u8,
+            ebss as *const () as usize - sbss as *const () as usize,
+        ).fill(0);
+    }
 }
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    mm::init();
     println!("[kernel] Hello, world!");
+    mm::init();
+    println!("[kernel] back to world!");
+    mm::remap_test();
     trap::init();
     trap::enable_timer_interrupt();
     timer::set_next_trigger();
-    loader::load_apps();
     task::run_first_task();
     panic!("Unreachable in rust_main!");
 }
